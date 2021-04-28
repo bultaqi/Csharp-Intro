63f199fc1bd9f7505d77a7971bb92dfa3c5f90bf
Gabriel <bultaqi@gmail.com>
2021-04-28 10:03:31 -0400

    first upload

---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    public class Customer
+    {
+        // Public Properties
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool IsLocal { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+
+    public class DeliveryService
+    {
+        /*
+          Properties
+        */
+        public string Name { get; set; }
+
+        public string TransitType { get; set; }
+
+        /*
+          Methods
+        */
+        public void Deliver(Product product, Customer customer)
+        {
+            Console.WriteLine($"Product delivered by {TransitType} to {customer.FullName}");
+        }
+    }
+
+    public class Product
+    {
+        /*
+          Properties
+        */
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        /*
+          Methods
+        */
+        public void Ship(Customer customer, DeliveryService service)
+        {
+            if (!customer.IsLocal)
+            {
+                service.Deliver(this, customer);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product tinkerToys = new Product()
+            {
+                Title = "Tinker Toys",
+                Description = "You can build anything you want",
+                Price = 32.49,
+                Quantity = 25
+            };
+
+            Customer marcus = new Customer()
+            {
+                FirstName = "Marcus",
+                LastName = "Fulbright",
+                IsLocal = false
+            };
+
+            DeliveryService UPS = new DeliveryService()
+            {
+                Name = "UPS",
+                TransitType = "train"
+            };
+
+            // Ship the tinker toys to Marcus using UPS
+            tinkerToys.Ship(marcus, UPS);
+        }
+    }
+}
+
+
+public class Box {
+    private string _secret = "Sometimes I sing Aretha Franklin songs in the shower.";
+
+    public string GetSecret(string magicWord) {
+        if (magicWord == "please") {
+            return _secret;
+        } else {
+            return "I'm not telling you!";
+        }
+    }
+}
+
+Box box = new Box();
+// The following line would cause an error. _secret is not accessible.
+// Console.WriteLine(box._secret);
+
+// But public methods and properties can access a private fields.
+Console.WriteLine(box.GetSecret("please"));
